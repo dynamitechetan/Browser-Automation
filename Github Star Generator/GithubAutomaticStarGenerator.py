c906206b4cf45d0abb916f3cb033469f0d41bf86
Anup Kumar Panwar <mylnext@gmail.com>
2016-07-16 16:20:29 +0530

    random names

--- Github Star Generator/GithubAutomaticStarGenerator.py
+++ Github Star Generator/GithubAutomaticStarGenerator.py
@@ -1,9 +1,14 @@
 import time
+import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-for x in range(13,8000000):
+for x in range(3,8000000):
+
+	i=random.randint(0,49);
+
+	names=["Nestor-Ohair", "Misty-Meiser", "Nyla-Hockman", "Evalyn-Aoki", "Michal-Mincks", "Maryalice-Railey", "Aubrey-Spengler", "Slyvia-Lucarelli", "Deedee-Bergquist", "Nu-Landrith", "Marilynn-Hansell", "Janelle-Seaberg", "Georgianne-Gott", "Shannan-Ota", "Raelene-Epperly", "Gabriela-Hodges", "Mathew-Samet", "Norah-Gin", "Toshia-Bergevin", "Mitsuko-Edmund", "Izola-Osmun", "Tonette-Vital", "Gwyneth-Blanchard", "Lashonda-Theriot", "Kecia-Alfrey", "Numbers-Tritt", "Lynsey-Bate", "Palmira-Aberle", "Jolynn-Gonyea", "Dino-Bulloch", "Lilliana-Pinkham", "Alyssa-Tice", "Thi-Rickards", "Diedra-Wiltsie", "Jayme-Haus", "Lazaro-Nicks", "Maybell-Perry", "Kandi-Weary", "Maryann-Schrom", "Rashad-Petrucci", "Chauncey-Silvis", "Flo-Eldredge", "Cora-Eber", "Lavelle-Medlen", "Lanelle-Blecha", "Genna-Holcomb", "Tyisha-Asmus", "Eleonora-Iverson", "Coleen-Daquila", "Craig-Beckmann"];
 
 	driver = webdriver.Chrome()
 
@@ -14,11 +19,11 @@ for x in range(13,8000000):
 	# driver.find_element_by_xpath("//*[@id='login']/p/a").click();
 
 	user  = driver.find_element_by_id("user_login");
-	user.send_keys("User-name12" + str(x));
+	user.send_keys(names[i] + str(x));
 	email = driver.find_element_by_id("user_email");
-	email.send_keys("User-name12" + str(x) + "@maildrop.cc");
+	email.send_keys(names[i] + str(x) + "@maildrop.cc");
 	passw = driver.find_element_by_id("user_password");
-	passw.send_keys("HelloWorld@123");
+	passw.send_keys(names[i]+"@123");
 	passw.send_keys(Keys.RETURN)
 	driver.find_element_by_xpath("//*[@id='js-pjax-container']/div/div[2]/div/form/button").click();
 
@@ -26,7 +31,10 @@ for x in range(13,8000000):
 
 	driver.get("https://github.com/AnupKumarPanwar/Ionic-FB-Login-PHP-MySQL-DB-AdMob");
 
+	driver.find_element_by_xpath("//*[@id='js-repo-pjax-container']/div[1]/div[1]/ul/li[3]/a[1]").click();
 	driver.find_element_by_xpath("//*[@id='js-repo-pjax-container']/div[1]/div[1]/ul/li[2]/div/form[2]/button").click();
+
+
 	# driver.find_element_by_xpath("//*[@id='user-links']/li[3]/div/div/form/button").click();
 
 	driver.quit();
